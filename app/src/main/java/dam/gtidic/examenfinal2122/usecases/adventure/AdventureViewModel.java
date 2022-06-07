3932c28823a13353d229565faf44cdba3a25ee87
Paula Uber <paula63033@gmail.com>
2022-06-07 16:44:26 +0200

    segon punt

--- app/src/main/java/dam/gtidic/examenfinal2122/usecases/adventure/AdventureViewModel.java
+++ app/src/main/java/dam/gtidic/examenfinal2122/usecases/adventure/AdventureViewModel.java
@@ -12,10 +12,19 @@ import dam.gtidic.examenfinal2122.utils.PreferencesProvider;
 
 public class AdventureViewModel  extends ViewModel {
     public MutableLiveData<String> adventure;
+    public MutableLiveData<Integer> qa_vides;
 
     //constructor
     public AdventureViewModel() {
         this.adventure = new MutableLiveData<>();
+        this.qa_vides = new MutableLiveData<Integer>();
+        qa_vides.setValue(0);
+    }
+
+    public void botonAdventure(){
+        //int vida = PreferencesProvider.providePreferences().getInt("numero",0);
+        int vida = qa_vides.getValue() + 1;
+        qa_vides.setValue(vida);
     }
 
 
